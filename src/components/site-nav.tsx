1d4974beb49efda94ddfc20fb5512c42016e7ae5
Anshu <sudhanshushekhar829@gmail.com>
2026-07-08 15:54:34 +0530

    components: add navigation layout skeleton

---
+++ src/components/site-nav.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+export function SiteNav() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-50 p-4 bg-black/50 text-white">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <span>Devstart</span>
+      </div>
+    </nav>
+  );
+}
