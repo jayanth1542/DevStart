5ccf834a90450aef82163319983c2eec7a8fecbd
Anshu <sudhanshushekhar829@gmail.com>
2026-07-08 15:54:34 +0530

    app: add dashboard page main container and layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen pt-24 pb-16 px-4 bg-black text-white">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
+    </div>
+  );
+}
